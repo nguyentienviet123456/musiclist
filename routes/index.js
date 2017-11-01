fcb85e1a170c6881b93f81015f01f870c684ca67
nguyentienviet123456 <nguyentienviet123456@gmail.com>
2017-11-01 11:49:01 +0700

    Initial Express

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,4 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-const testJSON = [
-  {
-    name: "viet",
-    username: "captain"
-  }, {
-    name: "viet",
-    username: "captain"
-  }
-];
-
-router.get('/sendjson', (req, res, next) => {
-  res.json(testJSON);
-});
-
 module.exports = router;
